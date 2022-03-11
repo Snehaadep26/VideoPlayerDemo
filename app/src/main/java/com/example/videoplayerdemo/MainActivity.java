bfd003880996395b16ba2dbccccb23e4912fef8e
Snehaadep26 <sneha.adep@brigosha.com>
2022-03-11 15:12:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/videoplayerdemo/MainActivity.java
@@ -0,0 +1,192 @@
+package com.example.videoplayerdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerControlView;
+import com.google.android.exoplayer2.ui.PlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean isShowingTrackSelectionDialog;
+    private DefaultTrackSelector trackSelector;
+    String[] speed = {"0.25x","0.5x","Normal","1.5x","2x"};
+    //demo url
+    String url1 = "https://5b44cf20b0388.streamlock.net:8443/vod/smil:bbb.smil/playlist.m3u8";
+
+    PlayerView playerView;
+    SimpleExoPlayer simpleExoPlayer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        trackSelector = new DefaultTrackSelector(this);
+        simpleExoPlayer = new SimpleExoPlayer.Builder(this).setTrackSelector(trackSelector).build();
+        playerView = findViewById(R.id.exoPlayerView);
+        playerView.setPlayer(simpleExoPlayer);
+        MediaItem mediaItem = MediaItem.fromUri(url1);
+        simpleExoPlayer.addMediaItem(mediaItem);
+        simpleExoPlayer.prepare();
+        simpleExoPlayer.play();
+
+
+        ImageView farwordBtn = playerView.findViewById(R.id.fwd);
+        ImageView rewBtn = playerView.findViewById(R.id.rew);
+
+        ImageView play=playerView.findViewById(R.id.exo_play);
+        ImageView pause=playerView.findViewById(R.id.exo_pause);
+
+
+
+
+
+
+        farwordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                simpleExoPlayer.seekTo(simpleExoPlayer.getCurrentPosition() + 10000);
+
+            }
+        });
+        rewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                long num = simpleExoPlayer.getCurrentPosition() - 10000;
+                if (num < 0) {
+
+                    simpleExoPlayer.seekTo(0);
+
+
+                } else {
+
+                    simpleExoPlayer.seekTo(simpleExoPlayer.getCurrentPosition() - 10000);
+
+                }
+
+
+            }
+        });
+
+        ImageView fullscreenButton = playerView.findViewById(R.id.fullscreen);
+        fullscreenButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SourceLockedOrientationActivity")
+            @Override
+            public void onClick(View view) {
+
+
+                int orientation = MainActivity.this.getResources().getConfiguration().orientation;
+                if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    // code for portrait mode
+
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                    findViewById(R.id.exo_play).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            simpleExoPlayer.play();
+
+                        }
+                    });
+                    findViewById(R.id.exo_pause).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            simpleExoPlayer.pause();
+
+                        }
+                    });
+
+
+                } else {
+                    // code for landscape mode
+
+                   // Toast.makeText(MainActivity.this, "Land", Toast.LENGTH_SHORT).show();
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                    findViewById(R.id.exo_play).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            simpleExoPlayer.play();
+
+                        }
+                    });
+                    findViewById(R.id.exo_pause).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            simpleExoPlayer.pause();
+
+                        }
+                    });
+
+                }
+
+
+            }
+        });
+
+
+        findViewById(R.id.exo_play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                simpleExoPlayer.play();
+
+            }
+        });
+        findViewById(R.id.exo_pause).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                simpleExoPlayer.pause();
+
+            }
+        });
+
+
+        simpleExoPlayer.addListener(new Player.Listener() {
+            @Override
+            public void onPlaybackStateChanged(int state) {
+                if (state == ExoPlayer.STATE_ENDED) {
+
+                }
+
+            }
+        });
+
+
+        playerView.setControllerVisibilityListener(new PlayerControlView.VisibilityListener() {
+            @Override
+            public void onVisibilityChange(int visibility) {
+
+            }
+        });
+
+
+
+
+
+    }
+}
